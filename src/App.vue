e1866d55e6760cee81f517140e7ce607b54388ab
esot0001 <esot0001@algonquinlive.com>
2024-06-07 06:29:10 -0400

    Initial file setup, created and displayed LearningResource component

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <ul>
+        <!-- benefits of creating a separate list component are: to help modularize the code, keep file from getting to bulky/cumbersome, and make finding and editing code easier -->
+        <learning-resource v-for="resource in storedResources" :key="resource.id" :title="resource.title" :description="resource.description" :link="resource.link"></learning-resource>
+    </ul>
+</template>
+
+<script>
+import LearningResource from "./components/learning-resources/LearningResource.vue";
+export default {
+    components: {
+        LearningResource
+    },
+    data () {
+        return {
+            storedResources: [
+                {
+                    id: "vue-guide",
+                    title: "Vue Guide",
+                    description: "Resource guide for vue",
+                    link: "https://vuejs.org/guide/quick-start.html"
+                },
+                {
+                    id: "google",
+                    title: "Google",
+                    description: "Resource guide for google",
+                    link: "https://google.com"
+                }
+            ]
+        }
+    }
+}
+</script>
---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,20 @@
+<template>
+    <li>
+        <div>
+            <header>
+                <h3>{{title}}</h3>
+                <button>Delete Resource</button>
+            </header>
+            <p>{{ description }}</p>
+            <nav>
+                <a :href="link">View Resource</a>
+            </nav>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    props: ["id", "title", "description", "link"]
+}
+</script>
