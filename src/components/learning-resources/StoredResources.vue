a47a69e4d314af7541859ab9f61a3c6ccd63fe6b
esot0001 <esot0001@algonquinlive.com>
2024-06-07 06:52:49 -0400

    Modularized code by displaying LearningResource in StoredResources and added some styling

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,27 @@
+<!-- This component removes the code to display the iterated list of learning resources from App.vue to make code more modular and also scope the style to the list -->
+<template>
+    <ul>
+        <!-- benefits of creating a separate list component are: to help modularize the code, keep file from getting to bulky/cumbersome, and make finding and editing code easier -->
+        <learning-resource v-for="resource in resources" :key="resource.id" :title="resource.title" :description="resource.description" :link="resource.link"></learning-resource>
+    </ul>
+</template>
+
+<script>
+import LearningResource from "./LearningResource.vue";
+export default {
+    components: {
+        LearningResource
+    },
+    props: ["resources"]
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
